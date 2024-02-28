466d31e60465ceacaee58b048586f27de19014e2
Ali Sadeghinia <asadeghinia@usf.edu>
2024-02-27 21:43:58 -0500

    MapReduce Update 6

--- Reducer_lab1.py
+++ Reducer_lab1.py
@@ -22,7 +22,18 @@ import sys
 dict_ip_count = {}
 
 for line in sys.stdin:
-    print line
+    line = line.strip()
+    hour, ip, num = line.split('\t')
+    try:
+        num = int(num)
+        dict_ip_count[(hour, ip)] = dict_ip_count.get(ip, 0) + num
+
+    except ValueError:
+        pass
+
+sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
+for ip, count in sorted_dict_ip_count:
+    print('%s\t%s' % (hour, ip, count))
 #     line = line.strip()
 #     hour, ip, num = line.split('\t')
 #     try:
