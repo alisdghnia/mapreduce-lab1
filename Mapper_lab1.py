ddd52d9d852a2fe304af15fa1a83643d6734ea63
Ali Sadeghinia <asadeghinia@usf.edu>
2024-02-27 20:50:22 -0500

    Mapper Update

--- Mapper_lab1.py
+++ Mapper_lab1.py
@@ -3,8 +3,26 @@
 import re
 import sys
 
+# pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
+# for line in sys.stdin:
+#     match = pat.search(line)
+#     if match:
+#         print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
+
+
+hashmap = {}
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
     match = pat.search(line)
     if match:
-        print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
+        hashmap[(match.group('hour'), match.group('ip'))] = hashmap.get((match.group('hour'), match.group('ip')), 0) + 1
+
+total_count = {}
+for (hour, ip), count in hashmap.items():
+    if hour in total_count:
+        total_count[hour] += count
+
+for hour, counts in total_count.items():
+    top3 = sorted([(count, ip) for (h, ip), count in hashmap.items() if h == hour], reverse=True)[:3]
+    for count, ip in top3:
+        print '%s\t%s\t%s' % (hour, ip, count)
